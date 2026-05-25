13cc9d2acd0d605c89a1e5b2ab60df55bbaea5fb
vvv <curtroan322@gmail.com>
2026-05-25 17:27:19 -0400

    config facets: fog_of_war rain terrain portal_gen city_gen

--- src/ankhor/facets/city_gen.js
+++ src/ankhor/facets/city_gen.js
@@ -1,1 +1,1 @@
-export default { priority:50, tick(_t,d,_dt,_r){ if(d._init)return; d._init=true; } };
+export default { priority:50, tick(_t,d,_dt,_r){ if(d._init)return; d._init=true; d.BLDG_HEIGHTS={shop:8,tower:25,house:6,garage:5,diner:7,bank:12,park:4,studio:10}; } };
--- src/ankhor/facets/fog_of_war.js
+++ src/ankhor/facets/fog_of_war.js
@@ -1,1 +1,1 @@
-export default { priority:50, tick(_t,d,_dt,_r){ if(d._init)return; d._init=true; } };
+export default { priority:50, tick(_t,d,_dt,_r){ if(d._init)return; d._init=true; d.FOW_RADIUS=25; d.FOW_FADE_START=18; d.FOW_OPACITY=0.85; } };
